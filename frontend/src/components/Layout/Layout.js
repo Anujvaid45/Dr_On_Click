6d3e64614856f5c975b60e460aede3241e4d0cb5
Anujvaid45 <vaidanuj80@gmail.com>
2023-04-24 22:52:13 +0530

    adding frontend folder

---
+++ frontend/src/components/Layout/Layout.js
@@ -0,0 +1,33 @@
+import Footer from "./Footer";
+import Header from "./Header";
+import {Helmet} from "react-helmet";
+import { ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+const Layout = ({children,title,description,author,keywords}) => {
+    return ( 
+        <div>
+            <Helmet>
+                <meta charSet="utf-8" />
+                <meta name="description" content={description} />
+                <meta name="keywords" content={keywords} />
+                <meta name="author" content={author} />
+                <title>{title}</title>
+            </Helmet>
+            <Header/>
+            <main style={{minHeight:'75vh'}}>
+                <ToastContainer autoClose={1000} />
+                {children}
+                </main>
+            <Footer/>
+        </div>
+    );
+}
+
+Layout.defaultProps = {
+    title:"Dr-On-Click",
+    description:'Medical',
+    keywords:"Medical",
+    author:'mind Benders',
+};
+ 
+export default Layout;
---
+++ frontend/src/pages/Auth/Register.js
@@ -0,0 +1,100 @@
+import React ,{useState} from 'react'
+import Layout from '../../components/Layout/Layout'
+import { toast } from 'react-toastify';
+import axios from 'axios'
+import { useNavigate} from 'react-router-dom';
+import '../../styles/AuthStyles.css';
+
+const Register = () => {
+
+    const[name,setName] = useState("")
+    const[email,setEmail] = useState("")
+    const[password,setPassword] = useState("")
+    const[phone,setPhone] = useState("")
+    const[address,setAddress] = useState("")
+    const[answer,setAnswer] = useState("")
+    const navigate = useNavigate()
+
+    const handleSubmit = async (e) =>{
+        e.preventDefault();
+        try {
+            const res = await axios.post('/api/v1/auth/register',
+            {name,email,password,phone,address,answer}
+            );
+            if(res.data.success){
+                toast.success(res.data.message)
+                setTimeout(()=>{navigate('/login')},2000)
+            }else{
+                toast.error(res.data.message)
+            }
+        } catch (error) {
+            console.log(error)
+            toast.error('Something went wrong')
+        }
+
+        
+
+    }
+
+    return (
+        <Layout title={'Register'}>
+            <div className="form-container">
+                <form onSubmit={handleSubmit}>
+                <h4 className="title">REGISTER FORM</h4>
+                <div className="mb-3">
+                        <input type="text" 
+                        placeholder='Enter Your Name'
+                        className="form-control"
+                        onChange={(e)=>setName(e.target.value)}
+                        value = {name}
+                        />
+                    </div>
+                    <div className="mb-3">
+                        <input type="email"
+                         className="form-control" 
+                         onChange={(e)=>setEmail(e.target.value)}
+                         placeholder='Enter Your Email'
+                         value = {email}
+                         />
+                    </div>
+                    <div className="mb-3">
+                        <input type="password"
+                         className="form-control"
+                         placeholder='Enter Your Password'
+                         onChange={(e)=>setPassword(e.target.value)}
+                         value = {password}
+                         />
+                    </div>
+                    <div className="mb-3">
+                        <input type="text"
+                         className="form-control" 
+                         placeholder='Enter Your Phone No.'
+                         onChange={(e)=>setPhone(e.target.value)}
+                         value = {phone}
+                          />
+                    </div>
+                    <div className="mb-3">
+                        <input type="text"
+                         className="form-control"
+                         placeholder='Enter Your Address'
+                         onChange={(e)=>setAddress(e.target.value)}
+                         value = {address}
+                         />
+                    </div>
+                    <div className="mb-3">
+                        <input type="text"
+                         className="form-control"
+                         placeholder='Who Is your favourite player'
+                         onChange={(e)=>setAnswer(e.target.value)}
+                         value = {answer}
+                         />
+                    </div>
+                    <button type="submit" className="btn btn-primary">REGISTER</button>
+                </form>
+            </div>
+        </Layout>
+
+    )
+}
+
+export default Register
